ad027adaa66f955d2bc0cda791a1aa96b20cf3e0
samtheman19 <makin.samuel@gmail.com>
2026-01-22 18:15:18 +0000

    Update app.js

--- app.js
+++ app.js
@@ -1,3 +1,8 @@
+if ('serviceWorker' in navigator) {
+  navigator.serviceWorker.getRegistrations().then(regs => {
+    regs.forEach(reg => reg.unregister());
+  });
+}
 document.addEventListener("DOMContentLoaded", () => {
 
   // -----------------------
